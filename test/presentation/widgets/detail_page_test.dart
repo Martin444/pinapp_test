ed102f3add0c4462f275cd35009799210078126a
mfarel <mfarel@openpass.com.ar>
2026-06-11 17:02:42 -0300

    add dependecies

--- test/presentation/widgets/detail_page_test.dart
+++ test/presentation/widgets/detail_page_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bloc_test/bloc_test.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:pinapp_dart_api/pinapp_dart_api.dart';
 import 'package:pinapp_test/presentation/blocs/comment_bloc/comment_bloc.dart';
 import 'package:pinapp_test/presentation/blocs/comment_bloc/comment_event.dart';
@@ -13,6 +12,7 @@ import 'package:pinapp_test/presentation/blocs/post_bloc/post_state.dart';
 import 'package:pinapp_test/presentation/ui/pages/detail_page.dart';
 
 class MockPostBloc extends MockBloc<PostEvent, PostState> implements PostBloc {}
+
 class MockCommentBloc extends MockBloc<CommentEvent, CommentState> implements CommentBloc {}
 
 void main() {
